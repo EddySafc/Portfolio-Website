3efd0fd092877bac312b08a1781d8fefe87d32f5
EddySafc <eddy@eddy-730QED>
2023-06-01 21:30:33 +0100

    changes to styling projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ const Projects = () => {
         <li id="project">
           Full Stack Game Reviews Project:{"  "}
           <p>
+            <p id="project-subheader">JS, React, PSQL</p>
             <a
               href="https://ncgamereviews-eddyking.netlify.app"
               id="project-link"
@@ -22,19 +23,65 @@ const Projects = () => {
           </p>
         </li>
         <li id="project">
-          Northcoders Group Project (Flutter):{"  "}
+          Full Stack Recipe Project:{"  "}
           <p>
+            <p id="project-subheader">JS, React, PSQL</p>
+            <a href="https://eddys-recipes.netlify.app/" id="project-link">
+              Eddys Rad Recipes
+            </a>
+            <p></p>
             <a
-              href="https://northcoders.com/projects/jan-2023/walkerrr"
+              href="https://github.com/EddySafc/MultiUse-PSQL-be"
               id="project-link"
             >
-              Walkerrr
+              Multi Use BE
             </a>
           </p>
         </li>
+
         <li id="project">
-          React Hangman project:{"  "}
+          Full Stack To Do List:{"  "}
           <p>
+            <p id="project-subheader">JS, React Native, PSQL</p>
+            <a
+              href="https://github.com/EddySafc/fe-react-native-todo-list"
+              id="project-link"
+            >
+              To Do List
+            </a>
+          </p>
+          <p></p>
+          <a
+            href="https://github.com/EddySafc/MultiUse-PSQL-be"
+            id="project-link"
+          >
+            Multi Use BE
+          </a>
+        </li>
+        <li id="project">
+          Full Stack Maths Game:{"  "}
+          <p>
+            <p id="project-subheader">JS, React Native, PSQL, Firebase</p>
+            <a
+              href="https://github.com/EddySafc/Native-Game-Firebase"
+              id="project-link"
+            >
+              Firebase Game
+            </a>
+          </p>
+          <p></p>
+          <a
+            href="https://github.com/EddySafc/MultiUse-PSQL-be"
+            id="project-link"
+          >
+            Multi Use BE
+          </a>
+        </li>
+
+        <li id="project">
+          Hangman project:{"  "}
+          <p>
+            <p id="project-subheader">JS, React</p>
             <a
               href="https://the-adventures-of-hangman.netlify.app/"
               id="project-link"
@@ -43,28 +90,11 @@ const Projects = () => {
             </a>
           </p>
         </li>
+
         <li id="project">
-          Full Stack Recipe Project:{"  "}
+          Team Picker Project:{"  "}
           <p>
-            <a href="https://eddys-recipes.netlify.app/" id="project-link">
-              Eddys Rad Recipes
-            </a>
-          </p>
-        </li>
-        <li id="project">
-          Full Stack To Do List Project:{"  "}
-          <p>
-            <a
-              href="https://github.com/EddySafc/fe-react-native-todo-list"
-              id="project-link"
-            >
-              To Do List
-            </a>
-          </p>
-        </li>
-        <li id="project">
-          React Native Team Picker Project:{"  "}
-          <p>
+            <p id="project-subheader">JS, React Native</p>
             <a
               href="https://github.com/EddySafc/Team-Pickle-fe"
               id="project-link"
@@ -74,19 +104,21 @@ const Projects = () => {
           </p>
         </li>
         <li id="project">
-          Full Stack Login Game Project:{"  "}
+          Northcoders Group Project (Flutter):{"  "}
           <p>
+            <p id="project-subheader">JS, Dart, Flutter, MongoDB, Firebase</p>
             <a
-              href="https://github.com/EddySafc/Native-Game-Firebase"
+              href="https://northcoders.com/projects/jan-2023/walkerrr"
               id="project-link"
             >
-              Firebase Game
+              Walkerrr
             </a>
           </p>
         </li>
         <li id="project">
           Multi Use PSQL BE:{"  "}
           <p>
+            <p id="project-subheader">PSQL</p>
             <a
               href="https://github.com/EddySafc/MultiUse-PSQL-be"
               id="project-link"
